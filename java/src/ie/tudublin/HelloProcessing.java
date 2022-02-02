d73a51ae06e3709bad7299ce352c9979ab590442
Hyper-TH <twila.shayne.habab@outlook.com>
2022-02-02 17:28:33 +0000

    Initial commit lab2

--- java/src/ie/tudublin/HelloProcessing.java
+++ java/src/ie/tudublin/HelloProcessing.java
@@ -10,23 +10,10 @@ public class HelloProcessing extends PApplet
 		size(500, 500);
 	}
 
-	public void setup() {
-		colorMode(HSB);
+	public void setup() { 
+		// colorMode(HSB); // HSB: Hue Saturation Brightness
+		colorMode(RGB);
 		background(0);
-
-		x1 = random(0, width);
-		x2 = random(0, width);
-		y1 = random(0, height);
-		y2 = random(0, height);
-
-		float range = 5;
-
-		x1dir = random(-range, range);
-		x2dir = random(-range, range);
-		y1dir = random(-range, range);
-		y2dir = random(-range, range);
-
-		smooth();
 		
 	}
 
@@ -36,32 +23,42 @@ public class HelloProcessing extends PApplet
 	
 	public void draw()
 	{	
-		strokeWeight(2);
-		stroke(c, 255, 255);
-		c = (c + 1f) % 255;
-		line(x1, y1, x2, y2);
+		// stroke(255); 
+		background(255, 0, 0); // single number is greyscale
+		// line(10, 10, 200, 200); // x1, y1, x2, y2 (origin at top left)
+		// circle(200, 250, 50); // cx, cy, r
+		// ellipse(50, 200, 90, 200); // cs, cy, w, h
 
-		x1 += x1dir;
-		x2 += x2dir;
-		y1 += y1dir;
-		y2 += y2dir;
+		// stroke(0);
+		// fill(0, 0, 255);
+		// noFill();
+		// noStroke();
+
+		// rectMode(CENTER);
+
+		// rect(300, 20, 100, 20); // tlx, tly, w, h
 		
-		if (x1 < 0 || x1 > width)
-		{
-			x1dir = - x1dir;
-		}
-		if (y1 < 0 || y1 > height)
-		{
-			y1dir = - y1dir;
-		}
+		// rectMode(CORNER);
+		// rect(200, 20, 100, 20); // tlx, tly, w, h
 
-		if (x2 < 0 || x2 > width)
-		{
-			x2dir = - x2dir;
-		}
-		if (y2 < 0 || y2 > height)
-		{
-			y2dir = - y2dir;
-		}
+		// point(20, 300);
+
+		// triangle(10, 400, 50, 450, 300, 200); // x1, y1, x2, y2, x3, y3
+	
+		fill(255, 255, 0);
+		noStroke();
+		circle(250, 270, 430);		
+
+		fill(0, 255, 255);
+		noStroke();
+		triangle(30, 450, 250, 10, 470, 450);
+
+		fill(128, 128, 128);
+		noStroke();
+		ellipse(250, 210, 190, 110);
+	
+		fill(0, 0, 0);
+		noStroke();
+		circle(250, 210, 100);
 	}
 }
