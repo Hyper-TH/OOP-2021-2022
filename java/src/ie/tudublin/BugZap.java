07e24dfcbcdc23bab93c06fd8772492e35916408
Hyper-TH <twila.shayne.habab@outlook.com>
2022-02-03 09:42:13 +0000

    bug moving

--- java/src/ie/tudublin/BugZap.java
+++ java/src/ie/tudublin/BugZap.java
@@ -6,6 +6,7 @@ public class BugZap extends PApplet {
 
     float playerX, playerY, playerWidth;
 
+    float bugX, bugY, bugWidth;
 	public void settings() {
 		size(500, 500);
 	}
@@ -13,9 +14,26 @@ public class BugZap extends PApplet {
     public void setup() {
         colorMode(RGB);
 
-        playerX == width / 2;
+        playerX = width / 2;
         playerY = height - 50;
         playerWidth = 50;
+
+        resetBug();
+        
+    }
+
+    private void resetBug() {
+        bugX = random(bugWidth / 2, width - (bugWidth / 2));
+        bugY = 50;
+        bugWidth = 50;
+    }
+
+    void drawBug(float x, float y, float w) {
+        float halfW = w / 2;
+
+        stroke(255);
+        noFill();
+        triangle(x - halfW, y + halfW, x, y - halfW, x + halfW, y + halfW);
     }
 
     void drawPlayer(float x, float y, float w) {
@@ -25,10 +43,32 @@ public class BugZap extends PApplet {
         line(x, y - 10, x, y - 20);
     }
 
+    float playerSpeed = 5;
+
+    public void keyPressed() {
+        
+        if (keyCode == LEFT) {
+            playerX -= playerSpeed;
+        }
+        if (keyCode == RIGHT) {
+            playerX += playerSpeed;
+        }
+    }
+
+    void moveBug() {
+        bugY ++;
+        bugX += random(-20, 20);
+    }
+
 	public void draw() {
         background(0);
         strokeWeight(2);
         drawPlayer(playerX, playerY, playerWidth);
+        drawBug(bugX, bugY, bugWidth);
+        
+        if (frameCount % 20 == 0) { // if multiple of 20
+            moveBug();
+        }
     }
 
 
