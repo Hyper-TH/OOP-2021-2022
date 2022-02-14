a1b34baf72a5442d1c54d76efeaf629b282ba2de
Hyper-TH <twila.shayne.habab@outlook.com>
2022-02-14 11:50:30 +0000

    Initial commit

--- java/src/ie/tudublin/Loops.java
+++ java/src/ie/tudublin/Loops.java
@@ -57,7 +57,7 @@ public class Loops extends PApplet
                 background(0);
                 int bars = 10;
                 float w = width /(float) bars;
-                float colorGap = 255 / (float) bars;
+                // float colorGap = 255 / (float) bars;
                 for (int i= 0; i < bars; i++) {
                     
                     noStroke();
@@ -65,8 +65,32 @@ public class Loops extends PApplet
                     rect(map(i, 0, 10, 0, 500), 0, w, height);
                 }
                 break;
-            case 1:
-                break;
+            
+			case 1:
+				// map(a, b c, d, e)
+				// a = input value
+				// b - c = start at the end of first range
+				// d - e =  start and end of the end range
+				
+				// map(-2, 10, 90, 200, 233);
+				break;
+			
+			case 2:
+				float gap = width * 0.1f;
+				float halfGap = gap / 2.0f;
+				colorMode(RGB);
+				stroke(0, 255, 0);
+				textAlign(CENTER, CENTER);
+				for(int i = -5 ; i <=5 ; i ++)
+				{
+					float x = map(i, -5, 5, gap, width -gap);				
+					line(x, gap, x, height - gap);
+					line(gap, x, width - gap, x);
+					fill(255);
+					text(i, x, halfGap);
+					text(i, halfGap, x);
+					
+				}
         }
     }
 }
