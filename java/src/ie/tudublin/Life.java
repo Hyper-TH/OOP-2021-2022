645654a4e1db6e30c31250be94c06fb646f12363
Hyper-TH <twila.shayne.habab@outlook.com>
2022-03-10 22:03:59 +0000

    bla

--- java/src/ie/tudublin/Life.java
+++ java/src/ie/tudublin/Life.java
@@ -2,18 +2,29 @@ package ie.tudublin;
 
 import processing.core.PApplet;
 
-public class Life extends PApplet {
+public class Life extends PApplet 
+{
+	LifeBoard board;
+	LifeBoard next;
 
-	public void settings() {
+	public void settings() 
+	{
 		size(500, 500);
+		board = new LifeBoard(100, this);
+		next = new LifeBoard(100, this);
+		board.randomise();
+		println(board.countCellsAround(1,1));
 	}
 
-	public void setup() {
-		colorMode(HSB);
+	public void setup() 
+	{
+		colorMode(RGB);
 
 	}
 
-	
-	public void draw() {
+	public void draw() 
+	{
+		board.render();
+		board.update();
 	}
 }
--- java/src/ie/tudublin/Main.java
+++ java/src/ie/tudublin/Main.java
@@ -33,6 +33,12 @@ public class Main
         processing.core.PApplet.runSketch( a, new Audio1());
     }
 
+    public static void life()
+	{
+		String[] a = {"MAIN"};
+        processing.core.PApplet.runSketch( a, new Life());
+    }
+
     public void cat()
     {
         System.out.println("Hello world");
@@ -58,7 +64,7 @@ public class Main
     }
     public static void main(String[] args)
     {
-        audio1();
+        bugZap();
 
         // Tara Misty
         // Tara Tara
